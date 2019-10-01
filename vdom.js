cc4fedee2e764095db76ad4d3e02e5750dbbc777
SHF <25377830+shff@users.noreply.github.com>
2019-10-01 18:58:14 -0300

    First commit

---
+++ vdom.js
@@ -0,0 +1,46 @@
+function h(element, props = {}, ..._children) {
+  return { element, props, _children };
+}
+
+function render(vnodes, dom) {
+  const nodes = [].concat(vnodes);
+  nodes.forEach((vnode, index) => {
+    const oldNode = dom.childNodes[index];oldNode
+    const state = oldNode && oldNode.element == vnode.element && oldNode.state || {};
+    const data = { element: vnode.element, state, ...vnode.props };
+
+    // Run components
+    while ((vnode.element || vnodes).bind) {
+      const setState = (newState) => Object.assign(state, newState) && render(vnodes, dom);
+      vnode = vnode.element(vnode.props, state, setState);
+    }
+
+    // Create a new element
+    let newNode = vnode.element
+      ? document.createElement(vnode.element)
+      : document.createTextNode(vnode);
+
+    // Reconcile
+    if (oldNode && oldNode.element != vnode.element && oldNode.data != vnode) {
+      dom.replaceChild(newNode, oldNode);
+    } else if (oldNode) {
+      newNode = oldNode;
+    } else {
+      newNode = dom.appendChild(newNode);
+    }
+
+    // Copy properties
+    Object.assign(newNode, data, { data: vnode });
+
+    // Render children
+    if (vnode._children && vnode._children.length) {
+      render(vnode._children, newNode);
+    }
+  });
+
+  // Remove unused children
+  while (dom.childNodes[nodes.length]) {
+    console.log(dom.childNodes[nodes.length])
+    dom.removeChild(dom.childNodes[nodes.length]);
+  }
+}
